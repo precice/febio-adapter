6c7701698912cef2111b0568341dc580b9df213f
Ishaan Desai <ishaandesai@gmail.com>
2023-05-12 14:38:15 +0200

    Updating 2d data generating script in example/

--- example/2d_generator/2d_generator.py
+++ example/2d_generator/2d_generator.py
@@ -16,21 +16,33 @@ solver_process_index = 0
 solver_process_size = 1
 interface = precice.Interface(participant_name, config_file_name, solver_process_index, solver_process_size)
 
-interface.initialize()
+mesh_name = "Generator-Mesh"
+mesh_id = interface.get_mesh_id(mesh_name)
+
+data_name = "Data"
+data_id = interface.get_data_id(data_name, mesh_id)
+
+positions = [[1, y0] for y0 in y[:-1]]
+
+vertex_ids = interface.set_mesh_vertices(mesh_id, positions)
+
+precice_dt = interface.initialize()
 
 t = 0
 
-while True:
+while interface.is_coupling_ongoing():
 
     dt = 0.01
 
     print("Generating data")
+    dt = np.minimum(dt, precice_dt)
     time.sleep(0.2)
     u = 1 - 2 * np.random.rand(n)
 
-    t = t + dt
-    if t > 0.1:
-        break
+    interface.write_block_scalar_data(data_id, vertex_ids, u)
 
+    precice_dt = interface.advance(dt)
+
+    t = t + dt
 
 interface.finalize()
