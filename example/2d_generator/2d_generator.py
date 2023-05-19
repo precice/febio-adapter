1c75465e32495d9715bbc5a0452761868e3a7103
Ishaan Desai <ishaandesai@gmail.com>
2023-05-19 09:38:17 +0200

    Add Z coords (0.0) to the 2D mesh in the 2d_generator because the problem itself is 3D

--- example/2d_generator/2d_generator.py
+++ example/2d_generator/2d_generator.py
@@ -10,12 +10,12 @@ data_id = interface.get_data_id("P_ext", mesh_id)
 np_axis = 10  # Number of points along one axis
 x_min, x_max = -0.5, 0.5
 y_min, y_max = -0.5, 0.5
-x_coords, y_coords = np.meshgrid(np.linspace(x_min, x_max, np_axis), np.linspace(y_min, y_max, np_axis))
+x_coords, y_coords, z_coords = np.meshgrid(np.linspace(x_min, x_max, np_axis), np.linspace(y_min, y_max, np_axis))
 
 dims = 2  # Dimensions of the generator mesh, even though the coupled problem is itself 3D
 nv = np_axis ** dims  # Total number of vertices
 
-coords = np.zeros((nv, dims))  # Coordinates of vertices, to set the coupling mesh in preCICE
+coords = np.zeros((nv, dims + 1))  # Coordinates of vertices, to set the coupling mesh in preCICE (1 layer in Z axis)
 write_data = np.zeros(nv)  # Data to write to preCICE in each time step
 
 # Use same number of points in x and y axis because we want a square mesh
@@ -24,6 +24,7 @@ for y in range(np_axis):
         n = x + y * np_axis
         coords[n, 0] = x_coords[x, y]
         coords[n, 1] = y_coords[x, y]
+        coords[n, 2] = 0.0
 
 vertex_ids = interface.set_mesh_vertices(mesh_id, coords)
 
